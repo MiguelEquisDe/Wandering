9bd5f40f14ff4ebe5feaf0e14608c48ce4cb625a
MiguelM <mamendez86@misena.edu.co>
2022-02-15 15:56:18 -0500

    modificando por 3ra vez :)

--- wandering.py
+++ wandering.py
@@ -7,4 +7,4 @@ class ComunWandering(Wandering):
     def __init__(self, name):
         super().__init__(name)
         def walk(self):
-            return random.choice([(0,1)(0,-1)(1,0)(-1,0)])
+            return random.choice([(0,2)(0,-2)(2,0)(-2,0)])
