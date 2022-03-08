84ae68241e03385c00f6cca17642322e957a4c3a
MiguelM <mamendez86@misena.edu.co>
2022-03-08 15:31:33 -0500

    aiudaa

--- track.py
+++ track.py
@@ -2,9 +2,21 @@ class Track:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+<<<<<<< HEAD
+       
+    def move(self, delt_x, delt_y):
+        return Track(self.x + delt_x, self.y + delt_y)
+   
+    def distance(self, other_track):
+        delt_x = self.x - other_track.x
+        delt_y = self.y - other_track.y
+       
+        return (delt_x**2 + delt_y**2)**0.5
+=======
         def move(self, delta_y, delta_x):
             return Track(self.x + delta_x, self.y + delta_y)
         def distance(self, other_track):
             delta_x = self.x - other_track.x
             delta_y = self.y - other_track.y
             return(delta_x**2 + delta_y**2)**0.5
+>>>>>>> random
--- wandering.py
+++ wandering.py
@@ -7,5 +7,11 @@ class  wandering:
 class ComunWandering(wandering):
     def __init__(self, name):
         super().__init__(name)
+<<<<<<< HEAD
+       
+    def walk(self):
+        return random.choice([(0,2),(0,-2),(2,0),(-2,0)])
+=======
     def walk(self):
         return random.choice([(0,3)(0,-3)(3,0)(-3,0)])
+>>>>>>> random
