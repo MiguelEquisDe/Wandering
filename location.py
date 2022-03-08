3c307d23c395b3ea484a155c8b59ebc29d96e4bc
MiguelM <mamendez86@misena.edu.co>
2022-03-08 15:40:34 -0500

    Vamos a guardar cambios

--- location.py
+++ location.py
@@ -1,5 +1,17 @@
 class Location:
     def __init__(self):
+<<<<<<< HEAD
+        self.location_wandering = {}
+       
+    def add_wandering(self, wandering, track):
+        self.location_wandering[wandering] = track
+       
+    def move_wandering(self,wandering):
+        delt_x, delt_y = wandering.walk()
+        location_now = self.location_wandering[wandering]
+        new_location = location_now.move(delt_x, delt_y)
+       
+=======
         self.location_de_wandering = {}
         
     def add_wandering(self, wandering, location):
@@ -9,6 +21,7 @@ class Location:
         delta_x, delta_y = wandering.walk()
         location_now = self.location_wandering(wandering)
         new_location = location_now.move(delta_x, delta_y)
+>>>>>>> random
         self.location_wandering[wandering] = new_location
    
     def get_location(self, wandering):
